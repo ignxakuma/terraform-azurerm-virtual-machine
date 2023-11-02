31404c78e602234662cd3ac953a2f9f038555203
ignxakuma <98381213+ignxakuma@users.noreply.github.com>
2023-11-02 17:13:39 +0530

    Create backend.tf

---
+++ examples/linux_virtual_machine/backend.tf
@@ -0,0 +1,8 @@
+terraform (
+ backend "azurerm" {
+    resource_group_name   = "Demo-rg"
+    storage_account_name  = "demo171998"
+    container_name        = "state"
+    key                   = "terraform.tfstate"
+  }
+}
